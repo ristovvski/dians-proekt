0ba3af304e4c3d4065d85c1b00fdc21d209318b0
Van40s <vanestojanov49@gmail.com>
2023-11-29 01:41:01 +0100

    Project Commit

---
+++ Домашно 2/CulturallyHistoricalObjectsWebApp/CulturallyHistoricalObjectsWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CulturallyHistoricalObjectsWebApp.Startup))]
+namespace CulturallyHistoricalObjectsWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
