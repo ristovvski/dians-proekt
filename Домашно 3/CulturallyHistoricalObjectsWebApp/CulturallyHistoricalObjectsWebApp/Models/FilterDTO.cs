56a84aa9046c6e8a713c74a4a2dae7cfda4affe6
Van40s <vanestojanov49@gmail.com>
2023-12-06 16:02:11 +0100

    Added original project to Домашно 3

---
+++ Домашно 3/CulturallyHistoricalObjectsWebApp/CulturallyHistoricalObjectsWebApp/Models/FilterDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CulturallyHistoricalObjectsWebApp.Models
+{
+    public class FilterDTO
+    {
+        public string name { get; set; }
+        public List<ObjectTypesModel> types {  get; set; }
+        public int? type_id {  get; set; }
+
+        public ObjectTypesModel ObjectTypesModel { get; set; }
+
+        public double userLatitude { get; set; }
+
+        public double userLongitude { get; set; }
+    }
+}
---
+++ Домашно 3/CulturallyHistoricalObjectsWebApp/CulturallyHistoricalObjectsWebApp/Models/HCObjectsDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CulturallyHistoricalObjectsWebApp.Models
+{
+    public class HCObjectsDTO
+    {
+        public String name {  get; set; }
+        public String type {  get; set; }
+        public double lon {  get; set; }
+        public double lat {  get; set; }
+    }
+}
